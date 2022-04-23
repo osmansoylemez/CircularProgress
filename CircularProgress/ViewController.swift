2663d97d2498a0135b8f7ece3d022cfe0d3d37bd
Osman Söylemez <osman.soylemez@turkiyefinans.com.tr>
2022-04-23 17:45:01 +0300

    Initial Commit

---
+++ CircularProgress/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CircularProgress
+//
+//  Created by Burcu Turan on 23.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
